5d1e7964bdb84de0975425f389dedc88a87c7f9d
Sander Kinds <sander.kinds@valtech.com>
2022-04-15 12:53:44 +0200

    Added Feature Flags in a Azure Function

--- src/Functions/Program.cs
+++ src/Functions/Program.cs
@@ -1,7 +1,28 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.FeatureFilters;
 
+var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings:AppConfig");
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration((context, builder) => builder.AddAzureAppConfiguration(options =>
+    {
+        options.Connect(connectionString)
+        .Select("_")
+        .UseFeatureFlags(options =>
+        {
+            options.Select(KeyFilter.Any, LabelFilter.Null);
+            options.Select(KeyFilter.Any, context.HostingEnvironment.EnvironmentName);
+            options.CacheExpirationInterval = TimeSpan.FromSeconds(3);
+        });
+    }))
+    .ConfigureServices(services =>
+    {
+        services.AddAzureAppConfiguration();
+        services.AddFeatureManagement().AddFeatureFilter<PercentageFilter>();
+    })
     .Build();
 
 host.Run();
